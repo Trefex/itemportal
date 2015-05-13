13a1787debbc3347cf50e88e0ecd4bb568cc1834
Trefex <trefex@gmail.com>
2015-05-13 22:23:31 +0200

    Updates

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,4 +13,25 @@ class Item < ActiveRecord::Base
             :default_url => ':default_image_url'
   validates_attachment_content_type :trainimage, :content_type => /\Aimage\/.*\Z/
 
+  # filterrific(
+  #     default_filter_params: { sorted_by: 'created_at_desc' },
+  #     available_filters: [
+  #       :sorted_by,
+  #       :search_query,
+  #       :with_country_id,
+  #       :with_created_at_gte
+  #     ]
+  #   )
+  #
+  #   # ActiveRecord association declarations
+  #   belongs_to :country
+  #
+  #   # Scope definitions. We implement all Filterrific filters through ActiveRecord
+  #   # scopes. In this example we omit the implementation of the scopes for brevity.
+  #   # Please see 'Scope patterns' for scope implementation details.
+  #   scope :search_query, lambda { |query|
+  #     # Filters students whose name or email matches the query
+  #     ...
+  #   }
+
 end
